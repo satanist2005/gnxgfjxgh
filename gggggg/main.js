351fc8dc1d4b35adce639cc02a98f47b97b9b17a
Satanist <82795576+satanist2005@users.noreply.github.com>
2024-02-08 15:03:47 +0300

    Add files via upload

---
+++ gggggg/main.js
@@ -0,0 +1,9 @@
+let user = {
+    login: 'user',
+    password:'123'
+}
+
+document.querySelector('.btn').addEventListener('click', (event)=>{
+    event.preventDefault();
+    console.log('asdasdasd')
+})
